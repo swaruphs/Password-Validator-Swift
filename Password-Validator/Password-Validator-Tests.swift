4ff515b0ed9524fdebba37353a91845938cab29e
Swarup <swaruphs@gmail.com>
2017-02-21 14:07:09 +0800

    Added check for Lowercase, uppercase and digits

--- Password-Validator/Password-Validator-Tests.swift
+++ Password-Validator/Password-Validator-Tests.swift
@@ -26,4 +26,55 @@ class Password_Validator_Tests: XCTestCase {
         XCTAssert(validator.isValid(password: "Hello"))
     }
     
+    
+    func testMinLengthValid() {
+        let validator = PasswordValidator.init(minLength: 6)
+        XCTAssert(validator.isValid(password: "Helloo") == true)
+    }
+    
+    func testMinLengthInvalid() {
+        let validator = PasswordValidator.init(minLength: 6)
+        XCTAssert(validator.isValid(password: "Hello") == false)
+    }
+    
+    func testMaxLengthValid() {
+        let validator = PasswordValidator.init(maxLength: 10)
+        XCTAssert(validator.isValid(password: "Hello") == true)
+    }
+    
+    func testMaxLengthInvalid() {
+        let validator = PasswordValidator.init(maxLength: 9)
+        XCTAssert(validator.isValid(password: "1234567890") == false)
+    }
+    
+    func testUppercasePasswordValid() {
+        let validator = PasswordValidator.init(rules: [.UpperCase])
+        XCTAssert(validator.isValid(password: "Hello") == true)
+    }
+    
+    func testUppercasePasswordInvalid() {
+        let validator = PasswordValidator.init(rules: [.UpperCase])
+        XCTAssert(validator.isValid(password: "hello") == false)
+    }
+    
+    func testLowercasePasswordValid() {
+        let validator = PasswordValidator.init(rules: [.LowerCase])
+        XCTAssert(validator.isValid(password: "HELLo") == true)
+    }
+    
+    func testLowercasePasswordInvalid() {
+        let validator = PasswordValidator.init(rules: [.LowerCase])
+        XCTAssert(validator.isValid(password: "HELLO") == false)
+    }
+    
+    
+    func testPasswordHasDigitValid() {
+        let validator = PasswordValidator.init(rules: [.Digit])
+        XCTAssert(validator.isValid(password: "Hello1"))
+    }
+    
+    func testPasswordHasDigitInValid() {
+        let validator = PasswordValidator.init(rules: [.Digit])
+        XCTAssert(validator.isValid(password: "hello") == false)
+    }
 }
--- Password-Validator/Password-Validator.swift
+++ Password-Validator/Password-Validator.swift
@@ -36,6 +36,54 @@ public struct PasswordValidator {
     
     public func isValid(password pwdString: String) -> Bool {
         
-        return false
+        if pwdString.characters.count < minLength {
+            return false
+        }
+        
+        if pwdString.characters.count > maxLength {
+            return false
+        }
+        
+        for rule in self.rules {
+            switch rule {
+            case .LowerCase:
+                
+                let lowercase = NSCharacterSet.lowercaseLetters
+                let hasLower = pwdString.unicodeScalars.contains { ( u: UnicodeScalar) -> Bool in
+                    lowercase.contains(u)
+                }
+                
+                if hasLower == false {
+                    return false
+                }
+            case .UpperCase:
+                let uppercase = NSCharacterSet.uppercaseLetters
+                let hasUpper = pwdString.unicodeScalars.contains { ( u: UnicodeScalar) -> Bool in
+                    uppercase.contains(u)
+                }
+                
+                if hasUpper == false {
+                    return false
+                }
+            case .Digit:
+                let digit = NSCharacterSet.decimalDigits
+                let hasDigit = pwdString.unicodeScalars.contains {( u: UnicodeScalar) -> Bool in
+                    digit.contains(u)
+                }
+                
+                if hasDigit == false {
+                    return false
+                }
+                
+                
+            default:
+                break
+            }
+        }
+        
+        return true
+        
+        
+        
     }
 }
